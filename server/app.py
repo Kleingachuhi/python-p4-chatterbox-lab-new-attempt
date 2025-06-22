6b0afc15e4ef2ba98ad4cbf7adaea81d784d8fd7
Klein <klein.gachuhi@student.moringaschool.com>
2025-06-22 11:08:20 +0300

    Completed Lab

--- server/app.py
+++ server/app.py
@@ -1,26 +1,45 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from flask_migrate import Migrate
-
 from models import db, Message
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-CORS(app)
-migrate = Migrate(app, db)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
+migrate = Migrate(app, db)
+CORS(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = Message.query.order_by(Message.created_at.asc()).all()
+        return jsonify([m.to_dict() for m in messages]), 200
+    elif request.method == 'POST':
+        data = request.get_json()
+        message = Message(body=data['body'], username=data['username'])
+        db.session.add(message)
+        db.session.commit()
+        return jsonify(message.to_dict()), 201
 
-@app.route('/messages/<int:id>')
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    if request.method == 'PATCH':
+        message = db.session.get(Message, id) 
+        if not message:
+            return jsonify({"error": "Message not found"}), 404
+        message.body = request.json['body']
+        db.session.commit()
+        return jsonify(message.to_dict())
+
+    elif request.method == 'DELETE':
+        message = db.session.get(Message, id)  
+        if not message:
+            return jsonify({"error": "Message not found"}), 404
+        db.session.delete(message)
+        db.session.commit()
+        return jsonify(message.to_dict())
 
 if __name__ == '__main__':
-    app.run(port=5555)
+    app.run(port=5000)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy import MetaData
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -12,3 +12,7 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text, nullable=False)
+    username = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now(), onupdate=db.func.now())
